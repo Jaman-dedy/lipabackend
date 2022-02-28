2b1a7719ae6a3d05e834c736ff1ab7f57e5bc87d
Rwabahizi Jonathan <jonathanrwabahizi@gmail.com>
2022-02-28 14:20:15 +0200

    filter transactions by user as admin

--- bitlipa/apps/transactions/managers.py
+++ bitlipa/apps/transactions/managers.py
@@ -35,11 +35,13 @@ class TransactionManager(models.Manager):
             table_fields.pop(key, None)  # remove fields not in the DB table
 
         if table_fields.get('source_address') and table_fields.get('source_address') == table_fields.get('target_address'):
-            query = models.Q(source_address=kwargs.get('source_address')) | models.Q(target_address=kwargs.get('target_address'))
+            query = models.Q(source_address__iexact=kwargs.get('source_address')) | \
+                models.Q(target_address__iexact=kwargs.get('target_address'))
             table_fields.pop('source_address', None)
             table_fields.pop('target_address', None)
-
-        query = models.Q(**{'deleted_at': None, **remove_dict_none_values(table_fields)})
+            query = query & models.Q(**{'deleted_at': None, **remove_dict_none_values(table_fields)})
+        else:
+            query = models.Q(**{'deleted_at': None, **remove_dict_none_values(table_fields)})
 
         if get_object_attr(user, "is_admin") and is_valid_uuid(kwargs.get('user_id')):
             query = query & (models.Q(sender_id=kwargs.get('user_id')) | models.Q(receiver_id=kwargs.get('user_id')))
