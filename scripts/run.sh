829f92ce7ef287c6205def208a192ab5aee3c389
Rwabahizi Jonathan <jonathanrwabahizi@gmail.com>
2022-03-20 22:58:41 +0200

    refactor firebase service

--- scripts/run.sh
+++ scripts/run.sh
@@ -24,6 +24,13 @@ if [[ -f "$(dirname "$0")/../.env.$env" ]]; then
   cat $(dirname "$0")/../.env.$env >$(dirname "$0")/../.env
 fi
 
+if [[ $port == "" ]]; then
+  port=$(grep -w .env -e 'PORT' | sed 's/PORT=//' | grep -v "#")
+fi
+if [[ $PORT ]]; then
+  port=$PORT
+fi
+
 python3 manage.py migrate
 python3 manage.py loaddata ./bitlipa/fixtures/*.json
 python3 manage.py runapscheduler &
